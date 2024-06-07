da11711be75c46a1d3bd203a5e2e2a447e82bf45
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2024-06-07 20:51:00 +0400

    o
    o

--- src/components/Home/AboutTwo/index.js
+++ src/components/Home/AboutTwo/index.js
@@ -14,14 +14,12 @@ const AboutTwo = () => {
         
         <h1 className='HeaderText' style={{color: '#d49400', border: '3px solid #d49400', zIndex: '1', position: 'absolute', background: '#000', padding: '5px 30px 5px 30px', width: '95%', textAlign: 'center'}}>Pure <span className='fontSpecial' style={{color: '#fff'}}>Delight </span> in Every Mango & Pickle</h1>
             
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', margin: '20px 0px', backgroundColor: '#000'}}>
+            <div className='makeColumn' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', margin: '20px 0px', backgroundColor: '#000'}}>
                 
-                <div className='hoverbw' style={{clipPath: 'polygon(0 0, 100% 0%, 80% 100%, 0% 100%)', display: 'flex', justifyContent: 'center', alignItems: 'flex-end', width: '50%', height: '100vh', backgroundColor: 'black', backgroundImage: `url('${Mangoes}')`, backgroundPosition: 'bottom', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-                    {/* <p className='fontSpecial' style={{fontSize: '30px', color: 'black', margin: '0', padding: '10px', width: '100%', textAlign: 'center', background: '#d49400'}}>Khas Aam</p> */}
+                <div className='hoverbw leftAboutTwo' style={{ backgroundImage: `url('${Mangoes}')`, backgroundPosition: 'bottom', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
                 </div>
 
-                <div className='hoverbw' style={{clipPath: 'polygon(20% 0%, 100% 0%, 100% 100%, 0 100%)', display: 'flex', justifyContent: 'center', alignItems: 'flex-end', width: '50%', height: '100vh', backgroundColor: 'black', backgroundImage: `url('${Pickles}')`, backgroundPosition: 'bottom', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-                    {/* <p className='fontSpecial' style={{fontSize: '30px', color: 'black', margin: '0', padding: '10px', width: '100%', textAlign: 'center', background: '#d49400'}}>Khas Achar</p> */}
+                <div className='hoverbw rightAboutTwo' style={{ backgroundImage: `url('${Pickles}')`, backgroundPosition: 'bottom', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
                 </div>
 
             </div>
